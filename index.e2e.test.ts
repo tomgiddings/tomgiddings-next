d5569a9769dd5b63ee25abd468fd70634515ecf5
Tom Giddings <tom.giddings@gmail.com>
2022-09-12 13:10:56 +0100

    update test

--- index.e2e.test.ts
+++ index.e2e.test.ts
@@ -1,7 +1,7 @@
 import * as TC from 'testcafe';
 
 TC.fixture`Getting Started`
-    .page`http://0.0.0.0:3000`;
+    .page`http://localhost:3000`;
 
     TC.test('My first test', async t => {
     // Test code
